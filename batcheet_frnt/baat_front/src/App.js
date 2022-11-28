ca62aafc1ea561c5f49705ca545f3c3b88d16ac1
pankajneema <pankaj200321@gmail.com>
2022-11-28 23:30:46 +0530

    2nd change Signed-off-by: pankajneema <pankaj200321@gmail.com>

--- batcheet_frnt/baat_front/src/App.js
+++ batcheet_frnt/baat_front/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import React, { useState } from "react";
 import { Modal, Button, Form } from "react-bootstrap";
+import login from "myComponants/login";
 
 
 
@@ -122,54 +123,13 @@ function App() {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  // const onLoginFormSubmit = (e) => {
-  //   e.preventDefault();
-  //   handleClose();
-  // };
-
-
   return (
-    // ````````````````````````````````````//
-   // -------------Login Card-------------//
-  //_____________________________________//
+   
 
-    <div className="">
-      <div className="row" id="allp">
-        <div className="col-2 " > </div>
-        <div className="col-4" id="firstlogin">
-          <div id="fst">
-            <p id="name" >BaatCheet</p>
-          </div>
-          <h4 id="line">Baatcheet helps you connect and share with the people in your life.</h4>
-        </div>
-        <div className="col-4">
-          <div id="login">
-            <div className="card" id="logincard" >
-              <div className="card-body">
-                <input name="name" type="text" className="form-control mb-3 form-control-lg" placeholder='username' aria-describedby="emailHelp" ></input>
-                <input name="password" type="text" className="form-control mb-3 form-control-lg " placeholder='password'></input>
-                <button type="button" className="btn btn-primary btn-lg btn-block mb-1"><strong>Log in</strong></button>
-                <a href='forgate' id="forgate" className="justify-content-center mb-4">Forgotten password?</a>
-                <hr id="hr"></hr>
-                <div id="lgnbt">
-                  <button type="submit" className="btn  form-control-lg" id="lgnbtn"  variant="primary" onClick={handleShow}><strong>Create New Account</strong></button>
-                  <Modal show={show} onHide={handleClose} id="modal">
-                    <Modal.Header closeButton >
-                      <Modal.Title className="mb-0 px-0">
-                        <h2>Sign Up</h2> 
-                       <small>It's quick and easy.</small>
-                      </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                      <LoginForm />
-                    </Modal.Body>
-                  </Modal>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div>
+      
+      <login/>
+
     </div>
       );
 
